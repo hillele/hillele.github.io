0b67ff5365e0b9653fd84d45ba96e25ae2ac5f4d
hillele <elexhill@gmail.com>
2017-03-28 00:42:16 -0700

    Update shows.js

--- dramap/shows.js
+++ dramap/shows.js
@@ -1,1 +1,1 @@
-var shows = [["Wolves", "http://www.brownpapertickets.com/event/2908686", "Undergraduate Theater Society", "The Cab", 47.659646, -122.306829, 03/25/2017, 03/29/2017, "Play"]];
+var rawShows = [["Wolves", "http://www.brownpapertickets.com/event/2908686", "Undergraduate Theater Society", "The Cab", 47.659646, -122.306829, 03/25/2017, 03/29/2017, "Play"]];
