fedde1c201034f86339636223eed4be7b79043d1
hillele <elexhill@gmail.com>
2017-03-28 17:16:30 -0700

    Update shows.js

--- dramap/shows.js
+++ dramap/shows.js
@@ -3,5 +3,5 @@ var rawShows = [
   ["Big Fish", "https://www.facebook.com/uwstagenotes/", "StageNotes", "Ethnic Cultural Theater", 47.655254, -122.314275, "05-25-2017", "05-28-2017", "Musical"],
   ["26 Miles", "http://westoflenin.com/index.php?option=com_content&view=article&id=156:26-miles&catid=9&Itemid=124", "Latino theatre Projects", "West of Lenin", 47.652480, -122.356345, "03-20-2017", "04-08-2017", "Play"],
   ["Dry Powder", "http://www.seattlerep.org/Plays/1617/DP/Synopsis", "Seattle Repertory Theatre", "Leo K. Theatre", 47.624244, -122.353284, "03-22-2017", "04-15-2017", "Play"],
-  ["Grounded", "https://seattlepublictheater.secure.force.com/ticket/#details_a0SF000000RVIDNMA5", "Seattle Public Theater", "Seattle Public Theater", 47.682112, -122.339819, "03-23-2017", "04-16-2017"]
+  ["Grounded", "https://seattlepublictheater.secure.force.com/ticket/#details_a0SF000000RVIDNMA5", "Seattle Public Theater", "Seattle Public Theater", 47.682112, -122.339819, "03-23-2017", "04-16-2017", "Play"]
 ];
